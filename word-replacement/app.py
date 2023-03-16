b977bc521d8910cc0807f9409787f2642496906d
Deepak Jha <11.deepakjha.dj@gmail.com>
2023-03-16 16:54:39 +0530

    Create app.py

---
+++ word-replacement/app.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    sentence = "Hi there! My name is Deepak."
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacemnent = input("Enter the replacement word: ")
+
+    print(sentence.replace(word_to_replace, word_replacemnent))
+
+replace_word()
